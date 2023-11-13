fbef91b21f8fa952c9123da971197b0062eea885
강승민 <rkdtmdals999@naver.com>
2023-11-13 11:35:07 +0900

    [🛠️feat] 메뉴 비즈니스 로직 구현

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,4 +1,41 @@
 package christmas.model;
 
+import static christmas.utility.Constants.COMMA;
+import static christmas.utility.Constants.LINE;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Menu {
+    private List<Map<MenuList, Integer>> order;
+    public Menu() {
+        this.order = new ArrayList<>();
+    }
+
+    public void setMenu(List<Map<MenuList, Integer>> order) {
+        this.order = order;
+    }
+
+    public void addOrder(String userInput) {
+        String[] menus = userInput.split(COMMA);
+        Map<MenuList, Integer> currOrder = new HashMap<>();
+        for (String menu : menus) {
+            String[] currMenu = menu.trim().split(LINE);
+            String menuName = currMenu[0].trim();
+            Integer quantity = Integer.parseInt(currMenu[1].trim());
+            addMenu(currOrder, menuName, quantity);
+        }
+        this.order.add(currOrder);
+    }
+
+    private void addMenu(Map<MenuList, Integer> order, String menuName, Integer quantity) {
+        for (MenuList menu : MenuList.values()) {
+            if (menu.getName().equals(menuName)) {
+                order.put(menu, quantity);
+                break;
+            }
+        }
+    }
 }
