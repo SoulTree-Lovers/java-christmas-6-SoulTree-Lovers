013812b28cba7a5d95126f2fae76ff6c02559a89
강승민 <rkdtmdals999@naver.com>
2023-11-13 10:29:41 +0900

    [🛠️feat] 메뉴 입력 메시지 출력 메소드 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -10,4 +10,8 @@ public class InputView {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
     }
+
+    public static void menu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
 }
