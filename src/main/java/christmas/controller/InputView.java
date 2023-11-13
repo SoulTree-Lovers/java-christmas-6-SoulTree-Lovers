8ba4f6ef2382a491e8fb8e1a80fc6776f9fc4894
강승민 <rkdtmdals999@naver.com>
2023-11-13 10:02:33 +0900

    [🛠️feat] Calendar 방문 날짜 클래스 변수 및 setter 추가

---
+++ src/main/java/christmas/controller/InputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class InputView {
+    public InputView() {}
+
+    public void printInputDate()  {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+}
--- src/main/java/christmas/model/Calendar.java
+++ src/main/java/christmas/model/Calendar.java
@@ -1,4 +1,11 @@
 package christmas.model;
 
 public class Calendar {
+    private static Integer visitDate;
+
+    public Calendar() {}
+
+    public static void setVisitDate(Integer visitDate) {
+        Calendar.visitDate = visitDate;
+    }
 }
