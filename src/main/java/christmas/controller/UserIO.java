057b90f3d5e13c77ee6d848149e862c2ef5ad7a8
강승민 <rkdtmdals999@naver.com>
2023-11-13 11:34:22 +0900

    [🛠️feat] 메뉴 입력 메소드 구현

--- src/main/java/christmas/controller/UserIO.java
+++ src/main/java/christmas/controller/UserIO.java
@@ -1,20 +1,42 @@
 package christmas.controller;
 
+import static christmas.utility.Constants.COMMA;
+
 import camp.nextstep.edu.missionutils.Console;
 import christmas.model.Calendar;
+import christmas.model.Menu;
+import christmas.model.MenuList;
 import christmas.view.InputView;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserIO {
-    public UserIO() {}
+    Calendar myCalendar;
+    Menu myMenu;
+    public UserIO() {
+        myCalendar = new Calendar();
+        myMenu = new Menu();
+    }
 
     public void InputVisitDate() {
         InputView.visitDate();
-        Calendar myCalendar = new Calendar();
         while (true) {
             try {
                 String userInput = Console.readLine();
                 Integer visitDate = Integer.parseInt(userInput);
                 myCalendar.setVisitDate(visitDate);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void InputMenu() {
+        InputView.order();
+        while (true) {
+            try {
+                String userInput = Console.readLine();
+                myMenu.addOrder(userInput);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
