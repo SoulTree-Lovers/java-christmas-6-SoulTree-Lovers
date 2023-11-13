5628856b43b1b662d915da36f3280cbe471d832c
강승민 <rkdtmdals999@naver.com>
2023-11-13 11:33:49 +0900

    [🎨style] 메소드 이름 변경 menu -> order

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
     }
 
-    public static void menu() {
+    public static void order() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
     }
 }
