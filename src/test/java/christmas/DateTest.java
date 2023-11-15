e5efa8c2afe4ae6df6f05a96e1e6ac6c7b3237aa
강승민 <rkdtmdals999@naver.com>
2023-11-15 19:56:55 +0900

    [🕹️test] ApplicationTest의 일부를 DateTest, OrderTest 클래스로 분리

---
+++ src/test/java/christmas/DateTest.java
@@ -0,0 +1,37 @@
+package christmas;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+public class DateTest extends NsTest {
+    @Test
+    void 날짜_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("a");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 날짜_예외_테스트_음수() {
+        assertSimpleTest(() -> {
+            runException("-1");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 날짜_예외_테스트_범위를_벗어난_날짜_입력() {
+        assertSimpleTest(() -> {
+            runException("40");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
---
+++ src/test/java/christmas/OrderTest.java
@@ -0,0 +1,53 @@
+package christmas;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+public class OrderTest extends NsTest {
+    @Test
+    void 주문_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-a");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 주문_예외_테스트_중복() {
+        assertSimpleTest(() -> {
+            runException("3", "타파스-1,제로콜라-1,제로콜라-2");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 주문_예외_테스트_없는_메뉴() {
+        assertSimpleTest(() -> {
+            runException("3", "떡볶이-1");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 주문_예외_테스트_메뉴_개수_음수() {
+        assertSimpleTest(() -> {
+            runException("3", "타파스--1");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 주문_예외_테스트_메뉴_개수_없음() {
+        assertSimpleTest(() -> {
+            runException("3", "타파스-0");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
